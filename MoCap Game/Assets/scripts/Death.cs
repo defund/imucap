eb5dd891e2492e14bb6cea8b5ba49b596c8e1fd9
Ryan Tse <rytse@mbhs.edu>
2016-10-01 23:22:14 -0400

    Move Robot Kyle to be child of camera instead of vice versa, disable first person controller, add elbow rotation, add (nonfunctional) death

---
+++ MoCap Game/Assets/scripts/Death.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Death : MonoBehaviour {
+
+    public Texture2D fadeOutTexture;
+    public float fadeSpeed = 0.8f;
+
+    private int drawDepth = -1000;
+    private float alpha = 1.0f;
+    private int fadeDir = -1;
+    private bool dead;
+
+
+	// Use this for initialization
+	void Start () {
+        dead = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (dead)
+            OnGUI();
+	}
+
+    void OnGUI()
+    {
+        alpha += fadeDir * fadeSpeed * Time.deltaTime;
+        alpha = Mathf.Clamp01(alpha);
+
+        GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alpha);
+        GUI.depth = drawDepth;
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeOutTexture);
+    }
+
+    void OnTriggerEnter()
+    {
+        dead = true;
+    }
+}
